be92a5cad3a984937c67938bde2feadada3e19a0
Vanessa <vanessa.nunez.p@gmail.com>
2017-05-15 22:16:20 -0300

    alteracoes feitas na faculdade

---
+++ docelandia-master/src/java/beans/Produto.java
@@ -0,0 +1,12 @@
+package beans;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable{
+    private int id;
+    private String nome;
+    private String descricao;
+    private int id_categoria;
+    private float preco;
+    private int quantidade;
+}
---
+++ src/java/beans/Categoria.java
@@ -0,0 +1,36 @@
+package beans;
+
+public class Categoria {
+    private int id_categoria;
+    private String nome;
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public int getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+}
