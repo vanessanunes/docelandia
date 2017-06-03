3fa42aa493be06a752a1088aeb478f1e88f7cb35
Vanessa <vanessa.nunez.p@gmail.com>
2017-06-03 11:34:33 -0300

    alteração no sql - tipo e id de user

--- MySQL Docelândia.sql
+++ MySQL Docelândia.sql
@@ -1,4 +1,4 @@
-
+﻿
 /*
 * @autor Bruno da Costa Silva
 * Projeto: Banco de Dados --> Docelândia
@@ -129,3 +129,14 @@ create table login (
 
 alter table login add constraint pk_login primary key (id_log);
 
+alter table login add tipo_user int;
+alter table login add id_user int;
+alter table login drop tipo;
+
+alter table telefone add tipo_user int;
+alter table telefone add id_user int;
+alter table telefone drop tipo;
+
+alter table endereco add tipo_user int;
+alter table endereco add id_user int;
+alter table endereco drop tipo;
