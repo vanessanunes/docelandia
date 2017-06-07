73a1daefbaaf3b28a949a13cd76481746e88db8c
Vanessa <vanessa.nunez.p@gmail.com>
2017-06-06 23:48:39 -0300

    adicionado funcoes aos produtos

--- src/java/models/ProdutoDAO.java
+++ src/java/models/ProdutoDAO.java
@@ -1,5 +1,4 @@
 package models;
-
 import beans.Produto;
 import conexao.ConnectionFactory;
 import java.sql.Connection;
@@ -25,15 +24,9 @@ public class ProdutoDAO {
         this.conexao = new ConnectionFactory().getConnection();
 
     }
-           
-        
-
-
-
     public void cadastraProduto(Produto produto){
         String sql = "insert into produto(id_prod, id_cat, nome, descricao, preco, quantidade)"
                 + "values (?, ?, ?, ?, ?, ?)";
-        
         try {
             try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
                 
@@ -51,8 +44,49 @@ public class ProdutoDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
+    
+    public void editarProduto(Produto produto){
+        String sql = "UPDATE produto"
+                + "SET nome = ?, descricao = ?, preco = ?, quantidade = ?, id_cat = ?"
+                + "WHERE id_prod = ?";
+        try {
+            try (PreparedStatement stmt = conexao.prepareStatement(sql)) {
+                
+                stmt.setString(1, produto.getNome());
+                stmt.setString(2, produto.getDescricao());
+                stmt.setDouble(3, produto.getPreco());
+                stmt.setInt(4, produto.getQuantidade());
+                stmt.setInt(5, produto.getId_cat());
+                
+                stmt.execute();
+                stmt.close();
+                status = ("Dados incluídos com sucesso!");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+        
+    public void excluirProduto(Produto produto) {
+        String sqlLogin = "delete from produto where id_prod = ?";
+
+        try {
+            try (PreparedStatement stmt = conexao.prepareStatement(sqlLogin)) {
+                stmt.setInt(1, produto.getId_prod());
+                stmt.execute();
+                stmt.close();
+                status = ("Produto excluído com sucesso!");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+    
 }
 
 
